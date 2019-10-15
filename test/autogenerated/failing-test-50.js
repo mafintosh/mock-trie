c70332291d26115133672ecd600fd68efeb68821
Andrew Osheroff <andrewosh@gmail.com>
2019-10-15 14:34:45 +0200

    Fuzzing bug fixes

--- test/autogenerated/failing-test-50.js
+++ test/autogenerated/failing-test-50.js
@@ -3,29 +3,27 @@ const Trie = require('../../trie')
 const Reference = require('../helpers/reference')
 
 async function applyOperations (trie) {
-  await trie.put('b/b/cb','jrtopmcslg')
-  await trie.rename('b','d')
   await trie.symlink('b/c/../../d/c/../b','b')
   await trie.rename('b','b')
   await trie.put('b/b','wwzwfblqhh')
 }
 function runTests () {
-  test('trie should return b/cb -> jrtopmcslg', async t => {
+  test('trie should return d/b/b -> wwzwfblqhh', async t => {
     const trie = await getTrie()
     await assertValid(t, trie)
   })
 
-  test('reference should return b/cb -> jrtopmcslg', async t => {
+  test('reference should return d/b/b -> wwzwfblqhh', async t => {
     const trie = await getReference()
     await assertValid(t, trie)
   })
 
   async function assertValid (t, trie) {
-    const node = await trie.get('b/cb')
-    const value = (node && node.value) ? node.value.value : null
+    const node = await trie.get('d/b/b')
+    const value = (node && node.value) ? node.value.value || node.value : null
 
     // Actually returns null
-    t.same(value, 'jrtopmcslg', 'getting b/cb returned jrtopmcslg')
+    t.same(value, 'wwzwfblqhh', 'getting d/b/b returned wwzwfblqhh')
     t.end()
   }
 }
