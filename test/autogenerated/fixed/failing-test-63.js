940b2f467c05c640d38b4d995b25566644353674
Andrew Osheroff <andrewosh@gmail.com>
2019-10-16 16:12:18 +0200

    New test + reference fix

---
+++ test/autogenerated/fixed/failing-test-63.js
@@ -0,0 +1,54 @@
+const test = require('tape')
+const Trie = require('../../../trie')
+const Reference = require('../../../fuzzing/reference')
+
+async function applyOperations (trie) {
+  await trie.symlink('/d','bc')
+  await trie.symlink('/bc','c')
+  await trie.put('d/db/d/b/cb','vbmnbwyfwc')
+  await trie.symlink('/d/c','b')
+  await trie.symlink('/b/cb/bc/d','c/c')
+  await trie.rename('d','b/c/b')
+}
+function runTests () {
+  test('trie should return d/c/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/c/b/db/d/b/cb -> vbmnbwyfwc', async t => {
+    const trie = await getTrie()
+    await assertValid(t, trie)
+  })
+
+  test.skip('reference should return d/c/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/c/b/db/d/b/cb -> vbmnbwyfwc', async t => {
+    const trie = await getReference()
+    await assertValid(t, trie)
+  })
+
+  async function assertValid (t, trie) {
+    const node = await trie.get('d/c/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/c/b/db/d/b/cb')
+    const value = (node && node.value) ? node.value.value || node.value : null
+
+    // Actually returns null
+    t.same(value, 'vbmnbwyfwc', 'getting d/c/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/cb/bc/d/c/b/db/d/b/cb returned vbmnbwyfwc')
+    t.end()
+  }
+}
+
+async function getTrie () {
+  const trie = new Trie()
+  await applyOperations(trie)
+  return trie
+}
+
+async function getReference () {
+  const reference = new Reference()
+  await applyOperations(reference)
+  return reference
+}
+
+if (require.main) {
+  runTests()
+} else {
+  module.exports = {
+    runTests,
+    getTrie,
+    getReference
+  }
+}
