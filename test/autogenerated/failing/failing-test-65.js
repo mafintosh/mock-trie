1a51592203b7d74b8bdebda91c8ae4d349ea91c0
Andrew Osheroff <andrewosh@gmail.com>
2019-10-16 16:25:18 +0200

    Test 65

---
+++ test/autogenerated/failing/failing-test-65.js
@@ -0,0 +1,52 @@
+const test = require('tape')
+const Trie = require('../../../trie')
+const Reference = require('../../../fuzzing/reference')
+
+async function applyOperations (trie) {
+  await trie.symlink('d/bd','b')
+  await trie.symlink('b/d','d')
+  await trie.put('c/c/b/c/d','wdzdsjtobv')
+  await trie.rename('d/bd','c')
+}
+function runTests () {
+  test('trie should return c/c/b/c/d -> null', async t => {
+    const trie = await getTrie()
+    await assertValid(t, trie)
+  })
+
+  test.skip('reference should return c/c/b/c/d -> null', async t => {
+    const trie = await getReference()
+    await assertValid(t, trie)
+  })
+
+  async function assertValid (t, trie) {
+    const node = await trie.get('c/c/b/c/d')
+    const value = (node && node.value) ? node.value.value || node.value : null
+
+    // Actually returns wdzdsjtobv
+    t.same(value, null, 'getting c/c/b/c/d returned null')
+    t.end()
+  }
+}
+
+async function getTrie () {
+  const trie = new Trie()
+  await applyOperations(trie)
+  return trie
+}
+
+async function getReference () {
+  const reference = new Reference()
+  await applyOperations(reference)
+  return reference
+}
+
+if (require.main) {
+  runTests()
+} else {
+  module.exports = {
+    runTests,
+    getTrie,
+    getReference
+  }
+}
