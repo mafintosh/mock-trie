7a34e8444cd71092c5c68b61b38538cd15268d0b
Mathias Buus <mathiasbuus@gmail.com>
2019-10-24 17:22:08 +0200

    add failing test

---
+++ test/autogenerated/failing/test-aca82703.js
@@ -0,0 +1,84 @@
+const p = require('path')
+const test = require('tape')
+const testSetup = require('reference-fuzzer/test')
+
+async function getObjects () {
+  const testingModule = require('../../../fuzzing.js')
+  const { actual, reference, state, executor: op, validators } = await testSetup(testingModule)
+
+  await op.symlink("../c","ddb")
+  await op.put("c/cd","dvodkpqesu")
+
+  return { actual, reference, state, tests: validators.tests }
+}
+
+function runTests () {
+  test('iterator should not return unexpected key ddb/cd', async t => {
+    const { tests } = await getObjects()
+
+    try {
+      await tests.sameIterators("ddb/cd",{"gt":false,"recursive":true})
+      t.pass('fuzz test passed')
+    } catch (err) {
+      if (err.longDescription) console.error(err.longDescription)
+      t.fail(err, 'iterator should not return unexpected key ddb/cd')
+    }
+    t.end()
+  })
+}
+
+module.exports = {
+  runTests,
+  getObjects,
+  config: {
+ "seed": "mock-trie15",
+ "numIterations": 2000,
+ "numOperations": 10,
+ "shortening": {
+  "iterations": 1000000
+ },
+ "inputs": {
+  "maxComponentLength": 3,
+  "maxPathDepth": 4
+ },
+ "operations": {
+  "put": {
+   "enabled": true,
+   "weight": 2
+  },
+  "symlink": {
+   "enabled": true,
+   "weight": 1
+  },
+  "rename": {
+   "enabled": true,
+   "weight": 1
+  },
+  "delete": {
+   "enabled": true,
+   "weight": 1
+  }
+ },
+ "validation": {
+  "reachable": {
+   "enabled": true
+  },
+  "syntheticKeys": {
+   "enabled": true,
+   "keys": 2000
+  },
+  "iterators": {
+   "enabled": true,
+   "keys": 200,
+   "recursive": true,
+   "gt": false
+  }
+ }
+},
+}
+if (require.main && !process.env['FUZZ_DISABLE_TEST']) {
+  runTests()
+}
+
+// Warning: Do not modify the signature below! It is used to deduplicate fuzz tests.
+// @FUZZ_SIGNATURE aca827035eef037f8fb4174d881b843d01e26f83cf7da7b1f91a61165e99df67
