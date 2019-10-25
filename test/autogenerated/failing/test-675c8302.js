409c2f43107488da5164d58e5e986c31323d281c
Mathias Buus <mathiasbuus@gmail.com>
2019-10-25 10:11:16 +0200

    add failing test

---
+++ test/autogenerated/failing/test-675c8302.js
@@ -0,0 +1,85 @@
+const p = require('path')
+const test = require('tape')
+const testSetup = require('reference-fuzzer/test')
+
+async function getObjects () {
+  const testingModule = require('../../../fuzzing.js')
+  const { actual, reference, state, executor: op, validators } = await testSetup(testingModule)
+
+  await op.symlink("/cb/bdd/dbd/bbc","c")
+  await op.symlink("/c","c/c/d/b")
+  await op.symlink("/c","c/b/b")
+
+  return { actual, reference, state, tests: validators.tests }
+}
+
+function runTests () {
+  test('Maximum call stack size exceeded', async t => {
+    const { tests } = await getObjects()
+
+    try {
+      await tests.sameIterators("cb",{"gt":false,"recursive":true})
+      t.pass('fuzz test passed')
+    } catch (err) {
+      if (err.longDescription) console.error(err.longDescription)
+      t.fail(err, 'Maximum call stack size exceeded')
+    }
+    t.end()
+  })
+}
+
+module.exports = {
+  runTests,
+  getObjects,
+  config: {
+ "seed": "mock-trie90",
+ "numIterations": 20000,
+ "numOperations": 100,
+ "shortening": {
+  "iterations": 1000000
+ },
+ "inputs": {
+  "maxComponentLength": 3,
+  "maxPathDepth": 4
+ },
+ "operations": {
+  "put": {
+   "enabled": true,
+   "weight": 2
+  },
+  "symlink": {
+   "enabled": true,
+   "weight": 1
+  },
+  "rename": {
+   "enabled": true,
+   "weight": 1
+  },
+  "delete": {
+   "enabled": true,
+   "weight": 1
+  }
+ },
+ "validation": {
+  "reachable": {
+   "enabled": true
+  },
+  "syntheticKeys": {
+   "enabled": true,
+   "keys": 2000
+  },
+  "iterators": {
+   "enabled": true,
+   "keys": 200,
+   "recursive": true,
+   "gt": false
+  }
+ }
+},
+}
+if (require.main && !process.env['FUZZ_DISABLE_TEST']) {
+  runTests()
+}
+
+// Warning: Do not modify the signature below! It is used to deduplicate fuzz tests.
+// @FUZZ_SIGNATURE 675c8302057312f906d608c49cf6c68b0c6d7e3f2b7ef3ff5545d43f0556304b
