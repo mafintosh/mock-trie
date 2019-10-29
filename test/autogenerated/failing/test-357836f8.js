aec195c031fb94b229193fcccfa655ded3e496e3
Mathias Buus <mathiasbuus@gmail.com>
2019-10-29 23:42:57 +0100

    add failing test

---
+++ test/autogenerated/failing/test-357836f8.js
@@ -0,0 +1,86 @@
+const p = require('path')
+const test = require('tape')
+const testSetup = require('reference-fuzzer/test')
+
+async function getObjects () {
+  const testingModule = require('../../../fuzzing.js')
+  const { actual, reference, state, executor: op, validators } = await testSetup(testingModule)
+
+  await op.put("d","rszhnjbzoh")
+  await op.symlink("c","ccd")
+  await op.symlink("d","bc/b")
+  await op.symlink("/bc/b","c")
+
+  return { actual, reference, state, tests: validators.tests }
+}
+
+function runTests () {
+  test('iterator should not return unexpected key ccd/ccb/dbb', async t => {
+    const { tests } = await getObjects()
+
+    try {
+      await tests.sameIterators("ccd",{"gt":false,"recursive":true})
+      t.pass('fuzz test passed')
+    } catch (err) {
+      if (err.longDescription) console.error(err.longDescription)
+      t.fail(err, 'iterator should not return unexpected key ccd/ccb/dbb')
+    }
+    t.end()
+  })
+}
+
+module.exports = {
+  runTests,
+  getObjects,
+  config: {
+ "seed": "mock-trie2231",
+ "numIterations": 10000,
+ "numOperations": 100,
+ "shortening": {
+  "iterations": 1000000
+ },
+ "inputs": {
+  "maxComponentLength": 3,
+  "maxPathDepth": 4
+ },
+ "operations": {
+  "put": {
+   "enabled": true,
+   "weight": 2
+  },
+  "symlink": {
+   "enabled": true,
+   "weight": 1
+  },
+  "rename": {
+   "enabled": true,
+   "weight": 1
+  },
+  "delete": {
+   "enabled": true,
+   "weight": 1
+  }
+ },
+ "validation": {
+  "reachable": {
+   "enabled": true
+  },
+  "syntheticKeys": {
+   "enabled": true,
+   "keys": 2000
+  },
+  "iterators": {
+   "enabled": true,
+   "keys": 200,
+   "recursive": true,
+   "gt": false
+  }
+ }
+},
+}
+if (require.main && !process.env['FUZZ_DISABLE_TEST']) {
+  runTests()
+}
+
+// Warning: Do not modify the signature below! It is used to deduplicate fuzz tests.
+// @FUZZ_SIGNATURE 357836f8a989c22cba886d0aa76a18e06e88659868c1b00bd51f6b1e817ba86d
